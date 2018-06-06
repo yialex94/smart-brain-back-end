f34e86ff2a39c398de2e28cf86d1b1cda14e9e6c
Alex Yi <alexyi@Alexs-MacBook-Pro.local>
2018-06-06 11:38:24 -0400

    editing database

--- server.js
+++ server.js
@@ -7,10 +7,8 @@ const knex = require('knex')
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : 'alexyi',
-    password : '',
-    database : 'smart-brain'
+    connectionString : process.env.DATABASE_URL,
+    ssl: true
   }
 });
 
